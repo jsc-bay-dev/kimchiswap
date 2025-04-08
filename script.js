4292a6664738dfa3b9942408db0f05bb3089d28e
= <=>
2025-04-08 11:38:53 -0700

    ethers

---
+++ script.js
@@ -0,0 +1,8 @@
+// Import everything
+import { ethers } from "ethers";
+
+// Import just a few select items
+import { BrowserProvider, parseUnits } from "ethers";
+
+// Import from a specific export
+import { HDNodeWallet } from "ethers/wallet";
